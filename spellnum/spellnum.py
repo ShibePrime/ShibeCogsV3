be4b0192681da0b6dec8ef258bcd4cbec3840d8d
therealshibe <andrewdkennedy1@gmail.com>
2018-07-15 11:30:27 -0700

    self.bot.say > ctx.send

--- spellnum/spellnum.py
+++ spellnum/spellnum.py
@@ -7,18 +7,15 @@ from redbot.core.utils import chat_formatting
 class spellnum:
     """Converts Words to Numbers and Numbers to words"""
 
-    def __init__(self, bot):
-        self.bot = bot
-
     @commands.command(pass_context=True)
-    async def spellnum(self, context, num):
+    async def spellnum(self, ctx, num):
         """Command for converting a number to words"""
         i = int(num)
         wordnum = num2words(i)
-        await self.bot.say(wordnum)
+        await ctx.send(wordnum)
 
     @commands.command(pass_context=True)
-    async def wordnum(self, context, word):
+    async def wordnum(self, ctx, word):
         """Command for converting words to numbers"""
         wordnum = w2n.word_to_num(word)
-        await self.bot.say(wordnum)
+        await ctx.send(wordnum)
