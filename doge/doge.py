e4bfd9092d1595ce250a49d7fca48cdb8b266f86
therealshibe <andrewdkennedy1@gmail.com>
2018-09-28 22:18:28 -0700

    a

--- doge/doge.py
+++ doge/doge.py
@@ -14,9 +14,6 @@ class doge:
     def __init__(self, bot):
         self.bot = bot
         self.base = 'data/doge/'
-        bot.settings.co_owners.append("90526038064402432")
-        bot.settings.co_owners.append("103627617260797952")
-        bot.settings.co_owners.append("90525689790365696")
 
     # --Start Much E-sports Image embeds , Images go in /data/images/
 
